f2a9859b06c12f7e658c6f8ecd2a4b4aeebba35f
Krish Patel <krishrpatel@gmail.com>
2024-06-08 11:07:01 -0400

    comments

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]); //todos is variable, setTodos is updating variable (array - of todos)
+  const [todo, setTodo] = useState('');//todo is variable, setTodo is updating variable (string - what the todo is)
 
   const addTodo = () => {
-    if (todo.trim()) {
-      setTodos([...todos, todo]);
-      setTodo('');
+    if (todo.trim()) { // makes sure todo is not empty string
+      setTodos([...todos, todo]); // fills todos with previous todos and the new todo
+      setTodo(''); //makes todo a blank string for the next todo to be added
     }
   };
 
+  // This function removes a todo from the todos based on position
   const removeTodo = (index) => {
-    const newTodos = todos.filter((_, i) => i !== index);
-    setTodos(newTodos);
+    const newTodos = todos.filter((_, i) => i !== index); // creates a new array without todo you want removed
+    setTodos(newTodos); // updates todos
   };
 
   return (
@@ -22,7 +23,7 @@ const TodoList = () => {
       <input
         type="text"
         value={todo}
-        onChange={(e) => setTodo(e.target.value)}
+        onChange={(e) => setTodo(e.target.value)} // updates todo variable with the val from input field
         placeholder="Add a new todo"
       />
       <button onClick={addTodo}>Add Todo</button>
@@ -37,5 +38,3 @@ const TodoList = () => {
     </div>
   );
 };
-
-export default TodoList;
